8c8b755846620325fc4fd8cb2440eb4c6d376c0e
Yushiro1113 <cgteoxon3970val@student.fatima.edu.ph>
2026-02-02 23:51:53 +0800

    first commit

---
+++ src/components/banner/data/data.js
@@ -0,0 +1,13 @@
+data/data.js 
+
+import img1 from '../images/chrome_hearts.jpg';
+import img2 from '../images/gallery_dept.jpg';
+import img3 from '../images/stussy.jpg';
+
+const bannerImages = [ 
+    img1,
+    img2,
+    img3,
+]
+
+export default bannerImages;
