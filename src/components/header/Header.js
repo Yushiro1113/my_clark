e614417adea85378e28f1d4007e18dab2ae263b1
Yushiro1113 <cgteoxon3970val@student.fatima.edu.ph>
2026-02-03 00:17:23 +0800

    first commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,6 +8,8 @@ export default function Header() {
         <div class="nav_item"><a>Home</a></div>
         <div class="nav_item"><a href="https://www.google.com" target="_blank">Google</a></div>
         <div class="nav_item"><a href="https://www.youtube.com" target="_blank">Youtube</a></div>
+        <div class="nav_item"><a href="https://www.instagram.com" target="_blank">Instagram</a></div>
+        <div class="nav_item"><a href="https://www.facebook.com" target="_blank">facebook</a></div>
       </div>
       <div class="login_button"><a>Login</a></div>
     </nav>
